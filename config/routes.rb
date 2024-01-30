43479f33b9208f5d3387f177dc71613d3c7ace01
Mariela IDOSSOU <marielaidossou@gmail.com>
2024-01-30 13:42:35 +0100

    ou
    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+  resources :bblogs
+  
+
+
+
   devise_for :users
 
   devise_scope :user do  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_24_152604) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_30_105225) do
+  create_table "bblogs", force: :cascade do |t|
+    t.text "custom_domain"
+    t.string "title"
+    t.string "theme"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "author_name"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_24_152604) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
