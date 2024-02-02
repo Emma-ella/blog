016741d6bed3424264584f9992508da1e064b5fe
Mariela IDOSSOU <marielaidossou@gmail.com>
2024-02-02 17:18:49 +0100

    sixth commit

--- db/migrate/20240202144335_add_bblog_to_posts.rb
+++ db/migrate/20240202144335_add_bblog_to_posts.rb
@@ -1,5 +1,5 @@
 class AddBblogToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :bblog, null: true, foreign_key: true
+    add_reference :posts, :bblog, null: false, foreign_key: true
   end
 end
